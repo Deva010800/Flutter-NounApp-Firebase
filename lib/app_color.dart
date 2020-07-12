e08942a09322a8d99d34703dc5a8aeae1cbe9378
Deva010800 <devabrilsana@gmail.com>
2020-07-12 12:15:13 +0800

    Flutter Firebase

---
+++ lib/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  final Color colorPrimary = Color(0xFFa4c638);
+  final Color colorSecondary = Color(0xFF87a232);
+  final Color colorTertiary = Color(0xFF6b7f2a);
+}
